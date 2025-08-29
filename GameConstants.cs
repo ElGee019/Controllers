28e995840ebbea48a57853e1fe16785a05879eb8
ElGee019 <allmightyligo@gmail.com>
2025-08-29 08:21:59 +0200

    Add files via upload

---
+++ GameConstants.cs
@@ -0,0 +1,29 @@
+
+public static class GameConstants
+{
+    // Car Save System
+    public const string CAR_POSITION_X = "SavedCarPosition_X";
+    public const string CAR_POSITION_Y = "SavedCarPosition_Y";
+    public const string CAR_ROTATION = "SavedCarRotation";
+    public const string CAR_FACING = "SavedCarFacing";
+    public const string CAR_STREET = "SavedCarStreet";
+    public const string CAMERA_HEIGHT = "SavedCameraHeight";
+
+    // Fuel System
+    public const string FUEL_SAVE_PREFIX = "CarFuel_";
+
+    // Insurance System
+    public const string CAR_INSURANCE_PREFIX = "CarInsurance_";
+    public const string INSURANCE_CLAIM = "InsuranceClaim";
+
+    // Car Ownership System
+    public const string CAR_OWNED_PREFIX = "CarOwned_";
+
+    // PlayerData
+    public const string PLAYER_NAME = "PlayerName";
+    public const string PLAYER_FAVORITE_BRAND = "PlayerFavoriteBrand";
+    public const string CURRENT_CAR = "CurrentCar";
+
+    // Intro Menu
+    public const string INTRO_MENU_COMPLETED = "IntroMenuCompleted";
+}
